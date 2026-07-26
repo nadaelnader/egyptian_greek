b8c7b58f2665bb988abab0e62472756b3514f3a9
nadaelnader <nadaelnader5@gmail.com>
2026-07-26 11:33:27 +0300

    model.py

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ Original file is located at
 from openai import OpenAI
 import streamlit as st
 
-client = OpenAI(
+client = openai(
     base_url="https://openrouter.ai/api/v1",
     api_key=st.secrets["OPENROUTER_API_KEY"]
 )
