e5af0266db41f98c059887c25c7902e37711b6cf
nadaelnader <nadaelnader5@gmail.com>
2026-07-26 12:16:13 +0300

    App.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
-from openai import OpenAI
 
 from retrieve import build_context
-from prompting import build_prompt, SYSTEM_PROMPT
-
+from model import ask_model
 
 # -------------------------
 # Page Configuration
@@ -13,100 +11,32 @@ st.set_page_config(
     page_icon="🏛️"
 )
 
-
-# -------------------------
-# OpenRouter Client
-# -------------------------
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=st.secrets["OPENROUTER_API_KEY"]
-)
-
-
 # -------------------------
 # UI
 # -------------------------
-st.title("🏛️ Egyptian & Greek Mythology AI")
-
-question = st.text_input(
-    "Ask about ancient mythology:"
-)
-
+st.title("Egyptian & Greek Mythology AI")
+question = st.text_input("Ask about ancient mythology:")
 
 # -------------------------
 # Generate Answer
 # -------------------------
 if question:
-
     with st.spinner("Searching mythology database..."):
-
         try:
-
-            # Retrieve context
             context, sources = build_context(question)
 
-
             if not context:
-
                 st.warning(
                     "I don't have enough information in the provided mythology database."
                 )
-
-
             else:
+                answer, sources = ask_model(question)
 
-                # Build RAG prompt
-                prompt = build_prompt(
-                    question,
-                    context
-                )
-
-
-                # Call Qwen model
-                response = client.chat.completions.create(
-
-                    model="qwen/qwen-2.5-7b-instruct",
-
-                    messages=[
-
-                        {
-                            "role": "system",
-                            "content": SYSTEM_PROMPT
-                        },
-
-                        {
-                            "role": "user",
-                            "content": prompt
-                        }
-
-                    ],
-
-                    temperature=0,
-                    top_p=0.1,
-                )
-
-
-                answer = response.choices[0].message.content
-
-
-                # Display Answer
                 st.subheader("Answer")
-
                 st.markdown(answer)
 
-
-                # Display Sources
                 with st.expander("Retrieved Sources"):
-
                     for doc in sources:
-
-                        st.markdown(
-                            f"**{doc['title']}**"
-                        )
-
-
+                        st.markdown(f"**{doc['title']}**")
         except Exception as e:
-
-            st.error(
-                f"Error: {e}"
-            )
+            st.error(f"Error: {e}")
